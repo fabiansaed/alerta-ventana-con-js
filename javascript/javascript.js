5abc5f61deb547308d5b1bd76f46b8f70633ccc4
fabiansaed <fabianojeda.1215@gmail.com>
2023-02-26 18:48:33 -0600

    Función alerta ventana con JS. Adición de estilos.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,15 @@
-console.log("Funcionando")
+//Guardo en una variable el título actual de mi página index.html
+let tituloActual = document.title
+
+//Esuchamos el evento 'blur' de la ventana
+window.addEventListener('blur', () =>{
+    tituloActual = document.title
+    //Cambiamos el título de la página
+    document.title = "🚀 NO TE VAYAS 🚀"
+})
+
+//Escuchamos el evento 'focus' de la ventana
+window.addEventListener('focus', () =>{
+    //Regresamos al título original
+    document.title = tituloActual
+})
